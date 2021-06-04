e72a2ffc7e89bfe205731a04c8a506fae555ffb1
Haytam <haytamkira@gmail.com>
2021-06-04 01:37:01 +0000

    fixing errors :skull:

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Blog } from "./components/Blog";
 import { getPosts } from "./actions/posts";
